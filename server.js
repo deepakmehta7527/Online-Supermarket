ae0f0da3a29d30cc54227189ca701d04d4220e38
Deepakmehta64 <deepakmehta7527@gmail.com>
2024-06-25 19:40:00 +0530

    modified

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 require('dotenv').config();
 // const dbstart=process.env.DB_LOCAL;
-const dbstart=process.env.db_server;
+const dbstart=process.env.DB_SERVER;
 
 mongoose
   .connect(dbstart)
